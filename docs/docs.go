565187d51575e8492e3cd8f96420428366a3837d
PhilLar <phillarov@gmail.com>
2019-11-08 10:29:11 +0300

    rm .idea

---
+++ docs/docs.go
@@ -0,0 +1,19 @@
+// Package classification go-chi_example.
+//
+// Documentation of our go-chi_example API.
+//
+//     Schemes: http
+//     BasePath: /
+//     Version: 1.0.0
+//     Host: some-url.com
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//
+//
+// swagger:meta
+package docs
